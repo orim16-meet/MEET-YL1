064cb319f7a20c710f04aabd7d9b3c3c8f5158c5
Guest <guest-Qv1679@meet.mit.edu>
2015-01-11 19:20:55 +0200

    class Animal

--- OOP.py
+++ OOP.py
@@ -1,19 +1,37 @@
 class Animal:
-	def __init__(self, name, color, age, size):
+	
+	
+	def __init__(self, name, color, age, size, oops):
 		self.name=name
 		self.color=color
 		self.age=age
 		self.size=size
+		self.oops=oops
 	def print_all(self):
 		print(self.name)
 		print(self.color)
 		print(self.age)
 		print(self.size)
-Snail = Animal(name="Arhimedes Ben-Amram", color="White", age=100, size="Large")
-Roni = Animal(name="Roni Ben-Amram", color="White", age=96, size="Large")
+		print(self.oops)
+	def eat(self):
+		print(self.name+":Oh it was delicious(I have ate)")	
+	def sleep(self):
+		print(self.name+":I have slept")
+
+	def Bathroom(self):
+		if  (self.oops==False):
+			print("Ohhh, Im glad I have went to the bathroom")
+		else:
+			print("oh, I have pooped, Muma!!!")
+			self.oops=True
+
+
+
+Snail = Animal(name="Arhimedes Ben-Amram", color="White", age=100, size="Large", oops=False)
+Mamuta = Animal(name="Gever Ben-Amram", color="White", age=96, size="Large", oops=True )
 
 
 Snail.print_all()
-print(BlaBlaBla)
-Roni.print_all()
+Snail.Bathroom()
 
+Snail.Bathroom()
